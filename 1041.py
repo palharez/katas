c5de8c11bd0f39efcc4817c6f8ae9808cc2daad4
Eduardo Palhares <xeduardopalhares@gmail.com>
2018-11-28 15:33:38 -0200

    Add 1041.py

---
+++ 1041.py
@@ -0,0 +1,31 @@
+entrada = input().split(" ")
+
+x, y = entrada
+x = float(x)
+y = float(y)
+
+if x > 0 and y > 0:
+    print("Q1")
+if x > 0 and y == 0:
+    print("Eixo X")
+
+if x < 0 and y == 0:
+    print("Eixo X")
+          
+if x > 0 and y < 0:
+    print("Q4")
+    
+if x < 0 and y > 0:
+    print ("Q2")
+
+if x < 0 and y < 0:
+    print ("Q3")
+
+if x == 0 and y < 0:
+    print("Eixo Y")
+
+if x == 0 and y > 0:
+    print("Eixo Y")
+
+if x == y == 0:
+    print("Origem")
