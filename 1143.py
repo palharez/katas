8681a45d37cb8af89e3512b75a0a56b180cb6f76
Eduardo Palhares <xeduardopalhares@gmail.com>
2018-11-29 15:16:08 -0200

    Add 1143.py

---
+++ 1143.py
@@ -0,0 +1,11 @@
+N = int(input())
+aux = 0
+for i in range(N):
+    aux += 1
+    for x in range(0, 3):
+        if (x == 0):
+            print(aux, end=" ")
+        elif (x == 1):
+            print(aux ** 2, end=" ")
+        else:
+            print(aux ** 3)
