bc481696dc9d150a674e8cc8db6a27a12b566424
Eduardo Palhares <xeduardopalhares@gmail.com>
2018-12-01 13:02:31 -0200

    Update 1178.py

--- vetores/1178.py
+++ vetores/1178.py
@@ -3,7 +3,7 @@ x = int(input())
 n = []
 n.append(x)
 
-print("N [%.4f] = %.4f" % (0, n[0]))
+print("N [%d] = %.4f" % (0, n[0]))
 for i in range(1, 100):
     n.append((n[i-1]) / 2)
     print("N [%d] = %.4f" % (i, n[i]))
