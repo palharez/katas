706c546ec2787ac64e79327ca88920844c62b6cd
palharez <xeduardopalhares@gmail.com>
2020-04-12 19:07:18 -0300

    add string repeat

---
+++ codewars/string_repeat.py
@@ -0,0 +1,8 @@
+def repeat_str(repeat, string):
+    return repeat * string
+
+
+if __name__ == "__main__":
+    print(repeat_str(4, 'a'), 'aaaa')
+    print(repeat_str(3, 'hello '), 'hello hello hello ')
+    print(repeat_str(2, 'abc'), 'abcabc')
