7484f157899b5fe81bbbcb006c47f840013851b4
palharez <xeduardopalhares@gmail.com>
2022-01-09 12:18:09 -0300

    add number of trailling spaces codewars kata

---
+++ codewars/number_of_trailling_zeros.py
@@ -0,0 +1,28 @@
+"""
+Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+
+N! = 1 * 2 * 3 * ... * N
+
+Be careful 1000! has 2568 digits...
+
+For more info, see: http://mathworld.wolfram.com/Factorial.html
+
+Examples
+zeros(6) = 1
+# 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
+
+zeros(12) = 2
+# 12! = 479001600 --> 2 trailing zeros
+Hint: You're not meant to calculate the factorial. Find another way to find the number of zeros.
+"""
+import math
+
+def zeros(n):
+    n = n if n <= 1 else n - 1
+    return str(math.factorial(n)).count("0")
+
+
+if __name__ == '__main__':
+    print(zeros(0), 0)
+    print(zeros(6), 1)
+    print(zeros(30), 7)
