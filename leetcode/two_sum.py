8810f059d6b7275b1ec0e5e55b0a6e140594a609
palharez <xeduardopalhares@gmail.com>
2023-01-31 10:34:51 -0300

    update two sum dp and bruteforce

--- leetcode/two_sum.py
+++ leetcode/two_sum.py
@@ -1,12 +1,35 @@
-def twoSum(nums, target):
-    dicto = {nums[i]: i for i in range(len(nums))}
+def twoSum_dp(nums, target):
+    """
+        Nos iteramos pelo array e verificamos se o valor que procuramos existe no array
+        A cada iteração nós preenchemos o array com I
+
+        We iterate over the array to verify if the needed value already exists in our
+        hash map.
+        In the end of each iteration we put I on hash map.
+
+        O(n)
+    """
+    hm = {}
+    for i, n in enumerate(nums):
+        diff = target - n
+
+        if diff in hm:
+            return [hm.get(diff), i]
+
+        hm[n] = i
+
+
+def twoSum_brute(nums, target) :
+    """
+    Time: O(n^2)
+    """
     for i in range(len(nums)):
-        value = target - nums[i]
-        if dicto.get(value):
-            if dicto[value] == i:
-                continue
-            return [i, dicto[value]]
+        for n in range(i + 1, len(nums)):
+            if nums[i] + nums[n] == target:
+                return [i, n]
 
 
 if __name__ == "__main__":
-    print(twoSum([1, 2, 3], 4))  # [0, 2]
+    assert twoSum_brute([1,2,3], 4) == [0, 2]
+    assert twoSum_dp([1,2,3], 4) == [0, 2]
+
